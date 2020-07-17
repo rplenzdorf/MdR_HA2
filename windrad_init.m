46fe497b9f9258a9efc025d34326d6e89a8b2bcc
rplenzdorf <53168242+rplenzdorf@users.noreply.github.com>
2020-07-17 13:32:49 +0200

    17.06.20 vormittag

--- windrad_init.m
+++ windrad_init.m
@@ -14,6 +14,7 @@ k = 11.5;
 Tt = 0.2;
 Kp = b/a
 T1 = 1/a
+T_PID = 0.002;
 
 P = T1/(Kp*Tt);
 
@@ -22,8 +23,27 @@ s = tf('s');
 Fs = b/(s+a)*exp(-Tt*s);
 Fr = 0.9*T1/(Kp*Tt)+1/(3.33*Tt*s);
 
+L = Fr*Fs;
 T = Fr*Fs/(1+Fr*Fs);
+S = 1/(1+Fr*Fs);
 
-margin(T)
+margin(L)
 
+Tt_r =38.6*(pi/180)/5.5
 %% Simulation
+sim("windrad_PID")
+sim("windrad")
+figure()
+plot(U)
+hold on
+plot(U_PID)
+set(gca, 'XScale', 'log')
+grid on
+hold off
+legend("PI","PID")
+figure()
+plot(alpha)
+hold on
+plot(alpha_PID)
+xlim([0 10])
+grid on
